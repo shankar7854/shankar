edaafcc1081a75a53bd813d821d9bbae0fa5dfb3
shankar7854 <shankarrh480@gmail.com>
2025-03-12 15:29:38 +0530

    Create count vowel.py

---
+++ count vowel.py
@@ -0,0 +1,11 @@
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+  
+    for char in s:
+        if char in vowels: 
+            count += 1
+    
+    return count
+result = count_vowels("Hello, World!")
+print(result)
