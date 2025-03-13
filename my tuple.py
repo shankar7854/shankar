adfb4d05aa125abeeccebfcd07c3c9d1bdc91931
shankar7854 <shankarrh480@gmail.com>
2025-03-13 15:05:52 +0530

    Create my tuple.py

---
+++ my tuple.py
@@ -0,0 +1,9 @@
+
+my_tuple = (10, 20, 30, 40, 50)
+
+
+print("Element at index 0:", my_tuple[0])  
+print("Element at index 1:", my_tuple[1])  
+print("Element at index 2:", my_tuple[2])  
+print("Element at index 3:", my_tuple[3])  
+print("Element at index 4:", my_tuple[4]) 
