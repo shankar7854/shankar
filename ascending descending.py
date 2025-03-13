ddec9f0e74fada5da123af0d6f9735f1800e4a21
shankar7854 <shankarrh480@gmail.com>
2025-03-13 14:57:01 +0530

    Create ascending descending.py

---
+++ ascending descending.py
@@ -0,0 +1,8 @@
+numbers = [50, 10, 30, 20, 40]
+
+ascending_order = sorted(numbers)
+
+descending_order = sorted(numbers, reverse=True)
+
+print("Sorted in ascending order:", ascending_order)
+print("Sorted in descending order:", descending_order)
