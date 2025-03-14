6309a6ed41840ceb6ad1ab25a1fc89ee1413c0ba
shankar7854 <shankarrh480@gmail.com>
2025-03-14 15:11:29 +0530

    Create class inheritance.1.py

---
+++ class inheritance.1.py
@@ -0,0 +1,13 @@
+class a:
+  def prajwal1(self):
+    print("prajwal1")
+class b:
+    def pradeep2(self):
+      print("pradeep2")
+
+class c (a,b):
+  pass
+
+obj = c()
+obj.prajwal1()
+obj.pradeep2()
