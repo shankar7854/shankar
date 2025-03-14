fe76c5b861a9665270d88942be6b6bda30d96144
shankar7854 <shankarrh480@gmail.com>
2025-03-14 12:35:00 +0530

    Create bank account .py

---
+++ bank account .py
@@ -0,0 +1,20 @@
+class BankAccount: 
+    def __init__(self, account_number, balance=0): 
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Desposited {amount}. New balance: {self.balance}")
+
+    def withdraw(self, amount): 
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+        else:
+            print("Insufficient balance")
+            
+
+account = BankAccount("15000", 1000) 
+account.deposit(1000)
+account.withdraw(500)
